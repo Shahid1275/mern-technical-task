711d07186194967b58c24e34a10c0fae3f9d36b3
SHAHID AMIN <156401527+Shahid1275@users.noreply.github.com>
2025-06-30 01:53:54 -0700

    Update authSlice.js

--- client/src/redux/features/authSlice.js
+++ client/src/redux/features/authSlice.js
@@ -7,7 +7,7 @@ export const registerUser = createAsyncThunk(
     try {
       const response = await axios.post(
         `${
-          https://mern-technical-task-1.onrender.com
+          import.meta.env.REACT_APP_API_URL || "http://localhost:3000"
         }/api/auth/register`,
         userData
       );
@@ -25,7 +25,7 @@ export const loginUser = createAsyncThunk(
     try {
       const response = await axios.post(
         `${
-            https://mern-technical-task-1.onrender.com
+          import.meta.env.REACT_APP_API_URL || "http://localhost:3000"
         }/api/auth/login`,
         credentials
       );
@@ -43,7 +43,7 @@ export const logoutUser = createAsyncThunk(
     try {
       const response = await axios.post(
         `${
-      https://mern-technical-task-1.onrender.com
+          import.meta.env.REACT_APP_API_URL || "http://localhost:3000"
         }/api/auth/logout`
       );
       return response.data;
